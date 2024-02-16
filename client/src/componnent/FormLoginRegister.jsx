2d3fa1e03b2d51657f13627b671c098b3af06af5
Rio-Rick <rioroderick@gmail.com>
2024-02-16 08:03:25 +0700

    commit aja

--- client/src/componnent/FormLoginRegister.jsx
+++ client/src/componnent/FormLoginRegister.jsx
@@ -140,7 +140,7 @@ export default function FormLoginRegister({handleSubmit , prop}) {
                         <GoogleLogin
                             onSuccess={googleLogin}
                             
-                            />;
+                            />
                     </div>
                     {/* <button
                     className="block w-full bg-indigo-600 py-2 rounded-2xl text-white font-semibold mb-2"
